0cc4fbfd28f2629357f393986973e3d41de23f48
Bryce Whittington <brycew30@gmail.com>
2020-06-19 16:16:10 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -18,10 +18,7 @@ def monopoly_with_second_tier
 	  :railroads=>{
 	    :pieces=>4,
 	    :names=>{},
-	    :rent_in_dollars=>
-	     {
-	      :one_piece_owned=>25
-	     }
+	    :rent_in_dollars=>{}
 	  }
 	}
 
@@ -32,7 +29,10 @@ def monopoly_with_third_tier
 	   :railroads=>{
 	     :pieces=>4,
 	     :names=>{},
-	     :rent_in_dollars=>{}
+	     :rent_in_dollars=>
+	      {
+	       :one_piece_owned=>25
+	      }
 	   }
 	 }
 
